f7d22de18d8f6fa9537a70a3eabfc50f934a913c
AngelGCL <angel.carrillo1@upr.edu>
2019-02-24 01:54:46 -0400

    deleted old files

--- dao/personDAO.py
+++ dao/personDAO.py
@@ -3,4 +3,32 @@ import psycopg2
 
 class PersonDAO:
 
-    personArray = [{"personID":1, "firstName":"Homero", "lastName":"Simpson", "phone":"1234567890", "email":"homerS@gmail.com", }]
+    personArray = [{"personID":1, "firstName":"Homero", "lastName":"Simpson", "phone":"1234567890", "email":"homerS@gmail.com",
+                   "birthday": "12/23/1968"}]
+
+    def getAllPersons(self):
+        return self.personArray
+
+    def getPersonByID(self, perid):
+        return self.personArray[0]
+
+    def getPersonByFName(self, perfname):
+        return self.personArray[1]
+
+    def getPersonByLName(self, perlname):
+        return self.personArray[2]
+
+    def getPersonByFullName(self, perfname, perlname):
+        return self.personArray[1], self.personArray[2]
+
+    def getPersonByEmail(self, permail):
+        return self.personArray[4]
+
+    def insert(self, json):
+        return self.personArray[1]
+
+    def update(self, perid, form):
+        return self.personArray[1]
+
+    def delete(self, perid):
+        return perid
---
+++ handler/persons.py
@@ -0,0 +1,44 @@
+from flask import jsonify
+from dao import personDAO
+
+class PersonsHandler:
+
+    def getAllPersons(self):
+        dao = personDAO()
+        person_list = dao.getAllPersons()
+        return jsonify(person_list)
+
+    def getPersonByID(self, perid):
+        dao = personDAO()
+        person = dao.getPersonByID(perid)
+        return jsonify(person)
+
+    def getPersonByFName(self, pfname):
+        dao = personDAO()
+        person_list = dao.getPersonByFName(pfname)
+        return jsonify(person_list)
+
+    def getPersonByLName(self, plname):
+        dao = personDAO()
+        person_list = dao.getPersonByLName(plname)
+        return jsonify(person_list)
+
+    def getPersonByFullName(self, pfname, plname):
+        dao = personDAO()
+        person_list = dao.getPersonByFullName(pfname, plname)
+        return jsonify(person_list)
+
+    def insertPersonJson(self, json):
+        dao = personDAO()
+        new_person = dao.insert(json)
+        return jsonify(new_person)
+
+    def updatePerson(self, perid, form):
+        dao = personDAO()
+        updated_person = dao.update(perid, form)
+        return jsonify(updated_person)
+
+    def deletePerson(self, perid):
+        dao = personDAO()
+        id = dao.delete(perid)
+        return jsonify(DeleteStatus = "OK"), 200
