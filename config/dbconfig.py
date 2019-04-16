6b18ae554e530a8470ed57bf7457f72d87ab7bed
raulcedres <raul.cedres@upr.edu>
2019-04-16 15:57:58 -0400

    DB credentials

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -2,7 +2,7 @@
 #Databse configuration information
 
 pg_config = {
-    'dbname': 'instapostdb',
-    'user': 'instapost',
-    'passwd': 'instapost'
+    'dbname': 'd5iv1130tgjcne',
+    'user': 'aqscsswljyuwzt',
+    'passwd': 'c2d062f76ea810737781a7e5c3ad459a2894ed170eb0f5a373ddcb7c7bf85f98'
 }
